259de3c9891eaf75a4d628f92a277f6c77c30a72
Tolulope <toluloperbrts@gmail.com>
2024-06-22 21:01:47 +0100

    Update image directory

--- src/components/ProfileContainer.tsx
+++ src/components/ProfileContainer.tsx
@@ -4,7 +4,7 @@ function ProfileContainer() {
   return (
     <div className={styles.card_container}>
       <img
-        src="/avatar-jessica.jpeg"
+        src="/public/assets/images/avatar-jessica.jpeg"
         className={styles.profile_image}
       />
       <h1>Jessica Randall</h1>
